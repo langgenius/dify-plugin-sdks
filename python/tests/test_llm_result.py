4ae8019bdab55ad2a51080c08014d3a0107b2185
Yeuoly <admin@srmxy.cn>
2025-04-11 14:37:31 +0800

    feat: add test for LLMResult to validate prompt_messages field and ensure backward compatibility

--- python/tests/test_llm_result.py
+++ python/tests/test_llm_result.py
@@ -1,9 +1,9 @@
-from dify_plugin.entities.model.llm import LLMResultChunk, LLMResultChunkDelta
+from dify_plugin.entities.model.llm import LLMResult, LLMResultChunk, LLMResultChunkDelta, LLMUsage
 from dify_plugin.entities.model.message import AssistantPromptMessage, TextPromptMessageContent
 
 
 def test_build_llm_result_chunk_with_prompt_messages():
-    prompt = LLMResultChunk(
+    chunk = LLMResultChunk(
         model="test",
         prompt_messages=[AssistantPromptMessage(content=[TextPromptMessageContent(data="Hello, World!")])],
         delta=LLMResultChunkDelta(
@@ -11,7 +11,7 @@ def test_build_llm_result_chunk_with_prompt_messages():
             message=AssistantPromptMessage(content=[TextPromptMessageContent(data="Hello, World!")]),
         ),
     )
-    assert isinstance(prompt.prompt_messages, list)
+    assert isinstance(chunk.prompt_messages, list)
     """
     NOTE:
     - https://github.com/langgenius/dify/issues/17799
@@ -20,4 +20,24 @@ def test_build_llm_result_chunk_with_prompt_messages():
     The `prompt_messages` field is deprecated, but to keep backward compatibility
     we need to always set it to an empty list.
     """
-    assert len(prompt.prompt_messages) == 0
+    assert len(chunk.prompt_messages) == 0
+
+
+def test_build_llm_result_with_prompt_messages():
+    result = LLMResult(
+        model="test",
+        prompt_messages=[AssistantPromptMessage(content=[TextPromptMessageContent(data="Hello, World!")])],
+        message=AssistantPromptMessage(content=[TextPromptMessageContent(data="Hello, World!")]),
+        usage=LLMUsage.empty_usage(),
+    )
+
+    assert isinstance(result.prompt_messages, list)
+    """
+    NOTE:
+    - https://github.com/langgenius/dify/issues/17799
+    - https://github.com/langgenius/dify-official-plugins/issues/648
+
+    The `prompt_messages` field is deprecated, but to keep backward compatibility
+    we need to always set it to an empty list.
+    """
+    assert len(result.prompt_messages) == 0
