30afaf71355c8916323610ba31c038ce44eeb04f
Yeuoly <admin@srmxy.cn>
2025-04-30 14:22:18 +0800

    fix: mkdocs

--- python/scripts/build_raw_docs.sh
+++ python/scripts/build_raw_docs.sh
@@ -9,8 +9,8 @@ PYTHON_SDK_DIR="$(dirname "${SCRIPT_DIR}")"
 function main {
     cd "${PYTHON_SDK_DIR}"
     pdm run python dify_plugin/cli.py generate-docs
-    mkdir -p docs
-    mv docs.md docs/schema.md
+    mkdir -p .mkdocs/docs
+    mv docs.md .mkdocs/docs/schema.md
 }
 
 main
