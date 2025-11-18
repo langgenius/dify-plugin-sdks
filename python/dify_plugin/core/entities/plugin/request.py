3bf597841b93893bd4706166c013b972b96c0f11
Yuichiro Utsumi <utsumi.yuichiro@fujitsu.com>
2025-11-18 00:08:25 +0000

    fix(text_embedding): pass input_type to distinguish between document and query
    Signed-off-by: Yuichiro Utsumi <utsumi.yuichiro@fujitsu.com>

--- python/dify_plugin/core/entities/plugin/request.py
+++ python/dify_plugin/core/entities/plugin/request.py
@@ -203,6 +203,7 @@ class ModelInvokeTextEmbeddingRequest(PluginAccessModelRequest):
     action: ModelActions = ModelActions.InvokeTextEmbedding
 
     texts: list[str]
+    input_type: EmbeddingInputType | None = EmbeddingInputType.DOCUMENT
 
 
 class ModelInvokeMultimodalEmbeddingRequest(PluginAccessModelRequest):
